bbfbfea2f06912ad9b9e491a37b4261707876df8
Lee Broxson <broxsonl@gmail.com>
2016-09-19 22:47:11 -0700

    Got DMs to work :D:D:D

--- lab-lee/server.js
+++ lab-lee/server.js
@@ -26,6 +26,18 @@ ee.on('\\all', function(client, string){
   });
 });
 
+ee.on('\\dm', function(client, string){
+  for (var i = 0; i < pool.length; i++) {
+    if (string.split(' ')[0] === pool[i].nickname) {
+      client.socket.write(`${client.nickname}(DM):` + string);
+      pool[i].socket.write(`(DM from ${client.nickname}):` + string);
+    }
+  }
+    // else {
+    //   client.socket.write(`Sorry, ${client.nickname}, that is not a valid user to DM.`);
+  // }
+});
+
 ee.on('default', function(client, string){
   client.socket.write('not a command');
 });
@@ -35,16 +47,22 @@ server.on('connection', function(socket){
   var client = new Client(socket);
   pool.push(client);
   socket.on('data', function(data) {
-    const command = data.toString().split(' ').shift.trim();
+    const command = data.toString().split(' ').shift().trim();
 
     if (command.startsWith('\\')) {
-      ee.emit(command, client, data.toString().split(' ').slice(1).join(' ')
+      ee.emit(command, client, data.toString().split(' ').slice(1).join(' '));
       return;
     }
     ee.emit('default', client, data.toString());
   });
+  socket.on('error', function(error) {
+
+  });
+  socket.on('close', function(){
+    ee.emit('');
+  });
 });
 
 server.listen(PORT, function(){
-  console.log('server running on port', PORT));
+  console.log('server running on port', PORT);
 });
