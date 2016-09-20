566dacd670bee3902aaacea23ee38a6c7108598c
Lee Broxson <broxsonl@gmail.com>
2016-09-20 02:06:55 -0700

    Some minor changes and cleanup.

--- lab-lee/model/client.js
+++ lab-lee/model/client.js
@@ -4,6 +4,6 @@ const uuid = require('uuid');
 
 module.exports = function(socket){
   this.socket = socket;
-  this.nickname = `user_${Math.round((Math.random() + 1)*(10))}`;
+  this.nickname = `user_${Math.round((Math.random() + 1)*(100))}`;
   this.id = uuid.v4().substring(0, 5);
 };
